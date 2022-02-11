e97c21daaeebe51996b745ccf39d7104b63fe45f
JEON-Sungsu <63297236+JEON-Sungsu@users.noreply.github.com>
2022-02-11 20:25:16 +0900

    page bg replace, main javascript anmation add

--- main.js
+++ main.js
@@ -16,6 +16,17 @@ $(function () {
   })
 })
 
+const shootEls = document.querySelectorAll('.shooting')
+
+shootEls.forEach(function (shootEl, index) {
+  gsap.to(shootEl, 1, {
+    x: 0,
+    y: 0,
+    opacity: 1,
+    delay: (index + 1) * 0.5,
+  })
+})
+
 const fadeEls = document.querySelectorAll('.fade-in')
 fadeEls.forEach(function (fadeEl, index) {
   gsap.to(fadeEl, 1, {
